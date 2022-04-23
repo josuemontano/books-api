a46e37c0f96ae2f72edee1aa65b229c5db7287d5
Josue Montano <josuemontanoa@gmail.com>
2022-04-22 23:06:24 -0400

    chore: create a function to generate cursors

--- api/graphql/helpers.py
+++ api/graphql/helpers.py
@@ -1,3 +1,9 @@
+import base64
 from typing import TypeVar
 
 GenericType = TypeVar("GenericType")
+
+
+def generate_cursor(identifier: str):
+    encoded = f"{identifier}".encode("utf-8")
+    return base64.b64encode(encoded).decode()
