4dea584a39a900258ec2e4e5ab7914c2b83b1cb3
Josue Montano <josuemontanoa@gmail.com>
2022-04-27 12:34:13 -0400

    chore: create the redis client

--- api/helpers/redis.py
+++ api/helpers/redis.py
@@ -1,5 +1,9 @@
 import os
 
+import aioredis
+
 redis_url = os.getenv("REDIS_URL", None)
 if redis_url is None:
     raise RuntimeError("REDIS_URL environment variable is not set")
+
+redis = aioredis.from_url(redis_url)
