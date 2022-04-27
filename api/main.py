ebdf4c8d1f175aaab2435bbc1177610d601031b8
Josue Montano <josuemontanoa@gmail.com>
2022-04-27 10:45:09 -0400

    chore: allow cross-origin requests

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 # Add code that uses environment variables AFTER THE NEXT LINE
 load_dotenv()
@@ -9,3 +10,5 @@ from .graphql import graphql_app
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
 app.add_websocket_route("/graphql", graphql_app)
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
