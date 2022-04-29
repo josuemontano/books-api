f314035eaea5f05a051a80e6dc3f309db6deb3a5
Josue Montano <josuemontanoa@gmail.com>
2022-04-28 22:00:07 -0400

    chore: add layout

--- client/ts/PiSeries.tsx
+++ client/ts/PiSeries.tsx
@@ -23,5 +23,22 @@ export default function PiSeries() {
     subscribe();
   }, []);
 
-  return <p>Pi: {value}</p>;
+  return (
+    <div>
+      <div>
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div />
+          <div className="px-4 py-5 bg-white dark:bg-slate-800 shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate">
+              Leibniz's series Pi
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-slate-300">
+              {value}
+            </dd>
+          </div>
+          <div />
+        </dl>
+      </div>
+    </div>
+  );
 }
